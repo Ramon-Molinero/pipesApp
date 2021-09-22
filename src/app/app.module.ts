f2ec3d21edf8284abe00472d911acf72c5331879
Ramón Molinero <molinero.gomez.ramon@gmail.com>
2021-09-22 13:17:25 +0200

    Animaciones, prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
@@ -24,7 +25,7 @@ registerLocaleData( localeFr);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AppRoutingModule,
+    BrowserAnimationsModule,
     SharedModule,
     VentasModule
   ],
